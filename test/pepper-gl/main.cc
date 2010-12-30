ef75454a67f27c136d49e94d201e97ebb6849ede
drhodes <physci@gmail.com>
2010-12-29 22:56:46 -0500

    reintroducing licenses

--- test/pepper-gl/main.cc
+++ test/pepper-gl/main.cc
@@ -1,19 +1,44 @@
-// include apple.copyright
 /*
-  #include <stdlib.h>
-  #include <stdio.h>
-  #include <nacl/nacl_npapi.h>
-  #include <pgl/pgl.h>
+  IMPORTANT:  This Apple software is supplied to you by Apple Computer, Inc.
+("Apple") in consideration of your agreement to the following terms, and
+your use, installation, modification or redistribution of this Apple software
+constitutes acceptance of these terms.  If you do not agree with these terms,
+please do not use, install, modify or redistribute this Apple software.
 
-  #include "plugin_object.h"
-  #include "event_handler.h"
-  #include "core.h"
+In consideration of your agreement to abide by the following terms, and
+subject to these terms, Apple grants you a personal, non-exclusive license,
+under Apple's copyrights in this original Apple software
+(the "Apple Software"), to use, reproduce, modify and redistribute the Apple
+Software, with or without modifications, in source and/or binary forms;
+provided that if you redistribute the Apple Software in its entirety and
+without modifications, you must retain this notice and the following text and
+disclaimers in all such redistributions of the Apple Software.  Neither the
+name, trademarks, service marks or logos of Apple Computer, Inc. may be used
+to endorse or promote products derived from the Apple Software without
+specific prior written permission from Apple. Except as expressly stated in
+this notice, no other rights or licenses, express or implied, are granted by
+Apple herein, including but not limited to any patent rights that may be
+infringed by your derivative works or by other works in which the Apple
+Software may be incorporated.
+
+The Apple Software is provided by Apple on an "AS IS" basis.  APPLE MAKES NO
+WARRANTIES, EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION THE IMPLIED
+WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+PURPOSE, REGARDING THE APPLE SOFTWARE OR ITS USE AND OPERATION ALONE OR IN
+COMBINATION WITH YOUR PRODUCTS.
+
+IN NO EVENT SHALL APPLE BE LIABLE FOR ANY SPECIAL, INDIRECT, INCIDENTAL OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) ARISING IN ANY WAY OUT OF THE USE, REPRODUCTION, MODIFICATION
+AND/OR DISTRIBUTION OF THE APPLE SOFTWARE, HOWEVER CAUSED AND WHETHER UNDER
+THEORY OF CONTRACT, TORT (INCLUDING NEGLIGENCE), STRICT LIABILITY OR
+OTHERWISE, EVEN IF APPLE HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
 #include <crml-core.h>
 #include <crml-evt.h>
 //#include <crml-win.h>
-
 
 #ifdef WIN32
 #define NPAPI WINAPI
@@ -102,18 +127,12 @@ NPError NPP_SetWindow(NPP instance, NPWindow* window) {
   if (obj){
     obj->SetWindow(*window);
 
-    /*  Danger Will Robinson DANGER DANGER DANGER.      
-        This is really awful, but it works.
-        Need to figure out a better way to do this when the next
-        sdk is released.
-    */
-    
+    /* crml changes.
+      Need to figure out a better way to do this when the next
+      sdk is released.
+    */   
     crml::Core::self_->SetPlugin(obj);
     crml::Core::self_->Check();
-    //crml::Core::self_->FirstRun();
-    /*
-      crml::Core::self_->MainLoop();
-    */
   }
   
   return NPERR_NO_ERROR;
